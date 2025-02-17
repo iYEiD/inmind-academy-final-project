afcaa9f8d50acc555a80ac2ddf414f3d8e72aa45
iYEiD <zeinoeid@gmail.com>
2025-02-17 18:03:06 +0200

    feat: add input and button component, add login module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { InputComponent } from './components/input/input.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [InputComponent, ButtonComponent],
+  imports: [CommonModule],
+  exports: [InputComponent, ButtonComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
