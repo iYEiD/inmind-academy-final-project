66600df4f94f6bd592d154c7b9f1fc7b1df2697f
iYEiD <zeinoeid@gmail.com>
2025-02-17 20:16:19 +0200

    feat: add footer component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [NavbarComponent],
-  imports: [CommonModule, RouterModule],
-  exports: [NavbarComponent],
+  declarations: [NavbarComponent, FooterComponent],
+  imports: [CommonModule, RouterModule, SharedModule],
+  exports: [NavbarComponent, FooterComponent],
 })
 export class CoreModule {}
--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrl: './button.component.scss'
+  styleUrl: './button.component.scss',
 })
 export class ButtonComponent {
-
+  @Input() modifiedButton: string = '';
 }
