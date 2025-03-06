fa849682bd86d613ecd1ad404e9b7523a8b8a187
iYEiD <zeinoeid@gmail.com>
2025-03-06 21:35:15 +0200

    feat: add image loading for product-details page

--- src/app/features/products/product-details/product-details.component.ts
+++ src/app/features/products/product-details/product-details.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { ProductsService } from '../services/products.service';
 import { ProductDTO } from '../../../models/product.model';
 import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { takeUntil, take } from 'rxjs/operators';
 import { switchMap, map, of, tap, shareReplay } from 'rxjs';
 import { ShoppingCartFacade } from '../../../features/shopping-cart/facades/shopping-cart.facade';
 import { CartMapper } from '../../../shared/mappers/cart.mapper';
@@ -16,6 +16,9 @@ import { CartMapper } from '../../../shared/mappers/cart.mapper';
 export class ProductDetailsComponent implements OnDestroy {
   quantity = 1;
   selectedImageIndex = 0;
+  imageLoading: boolean = true;
+  thumbnailsLoading: boolean[] = [];
+
   private cartFacade = inject(ShoppingCartFacade);
   private cartMapper = inject(CartMapper);
   route = inject(ActivatedRoute);
@@ -32,7 +35,12 @@ export class ProductDetailsComponent implements OnDestroy {
     }),
     map((response) => response?.products[0]),
     tap((product) => {
-      if (product) this.selectedImageIndex = 0; // Reset image index
+      if (product) {
+        this.selectedImageIndex = 0; // Reset image index
+        this.imageLoading = true; // Reset main image loading state
+        this.thumbnailsLoading = product.images.map(() => true); // Initialize loading state for all thumbnails
+        this.preloadImage(product.images[0]); // Preload the main image
+      }
     }),
     shareReplay(1)
   );
@@ -51,8 +59,47 @@ export class ProductDetailsComponent implements OnDestroy {
     this.destroy$.complete();
   }
 
+  preloadImage(src: string): void {
+    if (!src) {
+      this.imageLoading = false;
+      return;
+    }
+
+    const img = new Image();
+    img.src = src;
+    img.onload = () => {
+      this.imageLoading = false;
+    };
+    img.onerror = () => {
+      this.imageLoading = false;
+    };
+  }
+
+  preloadThumbnail(src: string, index: number): void {
+    if (!src) {
+      this.thumbnailsLoading[index] = false;
+      return;
+    }
+
+    const img = new Image();
+    img.src = src;
+    img.onload = () => {
+      this.thumbnailsLoading[index] = false;
+    };
+    img.onerror = () => {
+      this.thumbnailsLoading[index] = false;
+    };
+  }
+
   selectImage(index: number) {
     this.selectedImageIndex = index;
+    this.imageLoading = true;
+
+    this.product$.pipe(take(1)).subscribe((product) => {
+      if (product && product.images[index]) {
+        this.preloadImage(product.images[index]);
+      }
+    });
   }
 
   incrementQuantity() {
