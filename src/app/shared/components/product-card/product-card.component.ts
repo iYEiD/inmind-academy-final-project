91ffac5931aa44d32724de046fb535032f96d252
iYEiD <zeinoeid@gmail.com>
2025-03-06 20:03:57 +0200

    feat: add image loading for product-card

--- src/app/shared/components/product-card/product-card.component.ts
+++ src/app/shared/components/product-card/product-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { ProductDTO } from '../../../models/product.model';
 import { Router } from '@angular/router';
 import { ShoppingCartFacade } from '../../../features/shopping-cart/facades/shopping-cart.facade';
@@ -9,13 +9,38 @@ import { CartMapper } from '../../mappers/cart.mapper';
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss',
 })
-export class ProductCardComponent {
+export class ProductCardComponent implements OnInit {
   @Input() product?: ProductDTO;
   @Input() isLoading: boolean = false;
+
+  imageLoading: boolean = true;
 
   router = inject(Router);
   private cartFacade = inject(ShoppingCartFacade);
   private cartMapper = inject(CartMapper);
+
+  ngOnInit(): void {
+    // If we have a product, preload the image
+    if (this.product && !this.isLoading) {
+      this.preloadImage(this.product.thumbnail);
+    }
+  }
+
+  preloadImage(src: string): void {
+    if (!src) {
+      this.imageLoading = false;
+      return;
+    }
+
+    const img = new Image();
+    img.src = src;
+    img.onload = () => {
+      this.imageLoading = false;
+    };
+    img.onerror = () => {
+      this.imageLoading = false;
+    };
+  }
 
   navigateToDetails() {
     if (!this.product) return;
