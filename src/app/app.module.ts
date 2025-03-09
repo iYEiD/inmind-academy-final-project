3a9a8d0b8ba631bd77d3d5134ef6b428e2d28b1b
iYEiD <zeinoeid@gmail.com>
2025-03-09 14:32:22 +0200

    feat: add root level check to set user state if accessToken exist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +14,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { userReducer } from './shared/states/user/user.reducer';
 import { EffectsModule } from '@ngrx/effects';
 import { UserEffects } from './shared/states/user/user.effects';
+import { AccountFacade } from './core/authentication/facades/account.facade';
+import { AuthService } from './core/authentication/services/auth.service';
+
+// Factory function for APP_INITIALIZER
+function initializeUserState(
+  authService: AuthService,
+  accountFacade: AccountFacade
+) {
+  return () => {
+    if (authService.isAuthenticated()) {
+      accountFacade.loadUserProfile();
+    }
+    return Promise.resolve();
+  };
+}
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -34,6 +50,12 @@ import { UserEffects } from './shared/states/user/user.effects';
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
+      multi: true,
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeUserState,
+      deps: [AuthService, AccountFacade],
       multi: true,
     },
   ],
